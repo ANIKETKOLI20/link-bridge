396a76ab51f411e19658fbcd2ff707edad524cb4
aniket <aniketkoli17884@gmail.com>
2024-08-08 20:14:51 +0530

    Prepare app for deployment

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';  
+import cookieParser from 'cookie-parser'; 
+import path from 'path'; 
 import { connectToMongoDB } from './db/connectToMongoDB.js';
 import authRoutes from './routes/auth.route.js';
 import linkRoutes from './routes/link.route.js';
@@ -17,12 +18,20 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser()); 
+const __dirname = path.resolve();
 
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/links', linkRoutes);
 
 const PORT = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, "/frontend/build"))); 
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectToMongoDB();

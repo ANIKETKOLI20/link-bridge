2f580a421caf86f068b24ced29d8b1012034d7ef
aniket <aniketkoli17884@gmail.com>
2024-08-08 20:23:39 +0530

    Handle deployment error

--- backend/server.js
+++ backend/server.js
@@ -26,11 +26,13 @@ app.use('/api/links', linkRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, "/frontend/build"))); 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build"))); 
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
